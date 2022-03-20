6a3b52ab416b7e51933049d477ec375392c45d24
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-03-21 00:51:32 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,21 @@
+''''give fibonacci numbers '''''
+
+x = int(input("up-to what number you want fibonacci series \n"))
+
+def fib():
+    a = 0
+    b, c = 1, 1
+    z = globals()['x']
+    if z == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        while c < z and a + b <= z:
+            c = a + b
+            a = b
+            b = c
+            print(c)
+            c += 1
+
+fib()
