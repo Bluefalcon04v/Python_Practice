a8faddd5fafc9ee41de1c511fb5ee79ad35f08e2
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-03-24 11:38:11 +0530

    Add files via upload

---
+++ small games/stone paper scissor game.py
@@ -0,0 +1,32 @@
+import random
+print('let\'s play Stone, Paper, Scissor.')
+
+def play():
+
+    user = input("what is your choice? \n"
+                     "p for paper, s for scissor, r for rock\n").lower()
+    bot = random.choice(['p', 's', 'r'])
+
+    while user != 's' and user != 'p' and user != 'r':
+            user = input("invalid input. Try again.").lower()
+
+    else:
+        if user == bot:
+             return "it's an tie"
+
+        if win (user, bot):
+            return 'you won!'
+
+        return 'you lost.'
+
+
+#r > s, s > p, p > r
+def win(user,bot):
+
+    if (user == 'r' and bot == 's') or (user == 's' and bot == 'p') or (user == 'p' and bot == 'r'):
+        return True
+
+i = 1
+while i <= 5:
+    print(play())
+    i +=1
