a9538dc4a436bd565791e0657f645fdfba999d60
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-03-31 18:04:18 +0530

    Update Guess computer random number.py

--- small games/Guess computer random number.py
+++ small games/Guess computer random number.py
@@ -5,13 +5,20 @@ import random
 def guess(x):
     random_number = random.randint(1, x)       # randint() will take random int from range
     guess = 0
-    while guess != random_number:
+    chances_left = 12
+    while guess != random_number and chances_left > 0:
         guess = int(input(f"guess a number between 1 to {x}: "))
+        chances_left = chances_left - 1
         if guess < random_number:
-            print("too low, guess again ")
+            print(f"too low, guess again , chances_left: {chances_left}")
         elif guess > random_number:
-            print("too high, guess again")         # it will break while loop when we will guess number correctly
-    print(f"congrats! you guessed the number '{random_number}' correctly!!")
+            print(f"too high, guess again, chances_left: {chances_left}")
+
+    # it will break while loop when we will guess number correctly or chances_left become zero. 
+    if chances_left == 0:
+        print(f"you loose, number was {random_number} ")
+    else:
+        print(f"congrats! you guessed the number '{random_number}' correctly!!")
 
 
 guess(1000)
