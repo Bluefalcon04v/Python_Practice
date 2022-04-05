8247273e68bd04770888647066d76b0294709328
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-04-05 17:57:00 +0530

    Add files via upload

---
+++ Binary search methods/binary_search.py
@@ -0,0 +1,31 @@
+""" binary search we will compare the number 'n' with the mid_value,
+to see if it's greater than or smaller than mid_value."""
+
+posn = 0
+
+
+def search(list, n):
+    l = 0
+    u = len(list) - 1
+    list.sort()
+    print ('sorted list in ascending order:', list)
+
+    while l <= u:
+        mid = (l + u) // 2
+        if list[mid] == n:
+            globals()['posn'] = mid
+            return True
+        else:
+            if list[mid] < n:
+                l = mid + 1
+            else:
+                u = mid - 1
+    return False
+
+
+list = [20, 4, 6, 8, 12, 10, 18, 14, 16, 18, 2]
+n = 8
+if search(list, n):
+     print(f"found {n} and it's position is ", posn+1)
+else:
+    print("not found")
