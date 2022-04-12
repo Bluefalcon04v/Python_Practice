6b5546752053f46e78d9a79ce80317043727d724
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-04-12 21:40:26 +0530

    Add files via upload

---
+++ Typing_speed_test.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+print("""Welcome to Typing Test
+to END the game type end\n""")
+score = 0
+i = 1
+while i <= 4:
+    random_letter = random.choice(string.ascii_letters)
+    print(random_letter)
+    inp = input()
+    if random_letter == inp:
+        print("correct input")
+        score +=1
+    elif inp == "end" or "END":
+        print("You have ended the Typing test ")
+        break
+    else:
+        print("wrong input")
+    
+    i += 1
+
+print(f"Your accuracy is: {score/4*100}%, Thankyou!")
