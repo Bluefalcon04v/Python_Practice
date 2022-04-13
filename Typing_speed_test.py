c30215aff044c27d5caed00672d95c1b43bc3efb
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-04-14 00:09:19 +0530

    Add files via upload

--- Typing_speed_test.py
+++ Typing_speed_test.py
@@ -1,23 +1,38 @@
+"""it is a speed typing game in which countdown is running in the background, and we have to type 20 words in given time"""
+
 import random
 import string
+from time import *
+import threading     # it will help us to run 2 or more program at the same time
 
-print("""Welcome to Typing Test
-to END the game type end\n""")
+
+def countdown():
+    global my_timer         # defining it as a global, so we can use it in while loop later
+    my_timer = 12           # +2 second is to read menu
+    for x in range(12):
+        my_timer -= 1
+        sleep(1)
+    print("\nout of time")
+
+countdown_thread = threading.Thread(target = countdown)
+countdown_thread.start()
+
+print("""Welcome to speed typing game.
+You have to type 20 words in 10 seconds with best accuracy as you can""")
 score = 0
 i = 1
-while i <= 26:
-    random_letter = random.choice(string.ascii_letters)
-    print(f"{i}) {random_letter}")
-    inp = input()
-    if random_letter == inp:
-        print("correct input")
-        score +=1
-    elif inp == "end" or "END":
-        print("You have ended the Typing test ")
-        break
-    else:
-        print("wrong input")
+global my_timer
+ 
+while my_timer > 0:
+    while i <= 20 and my_timer != 0:
+        random_letter = random.choice(string.ascii_letters)
+        print(f"{i}) {random_letter}")
+        inp = input()
+        if random_letter == inp:
+            print("correct input")
+            score +=1
+        else:
+            print("wrong input")
+        i += 1
 
-    i += 1
-
-print(f"Your accuracy is: {score/26*100}%, Thankyou!")
+print(f"Your score is: {score} \nYour accuracy is: {score/20*100}%, Thankyou!")
